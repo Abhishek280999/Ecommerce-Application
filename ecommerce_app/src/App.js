3134eacb6e9250db402d5c029c376323c67644e3
Abhishek280999 <darwekar155@gmail.com>
2023-08-15 21:39:43 +0530

    image add

--- ecommerce_app/src/App.js
+++ ecommerce_app/src/App.js
@@ -4,6 +4,9 @@ import Layout from './components/Layout';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Shop from './Pages/Shop';
+import Blog from './Pages/Blog';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
         <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
         <Route path='shop' element={<Shop/>}/>
+        <Route path='blog' element={<Blog/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
+
         </Route>
       </Routes>
      </BrowserRouter>
--- ecommerce_app/src/components/Header.js
+++ ecommerce_app/src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BiPhoneCall } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
-import {AiOutlineMail} from 'react-icons/ai'
+import { AiOutlineMail } from 'react-icons/ai'
+import Logo from '../Images/logo.png'
+import {HiOutlineArchiveBox}  from 'react-icons/hi'
 
 const Header = () => {
     return (
@@ -18,14 +20,48 @@ const Header = () => {
                             </div>
                             <div>
                                 <Link><BiPhoneCall className='fs-3 mx-4' /></Link>
-                                <Link><AiOutlineMail className='fs-3 mx-4'/></Link>
+                                <Link><AiOutlineMail className='fs-3 mx-4' /></Link>
                             </div>
                         </div>
                     </div>
                 </div>
             </header>
 
-            
+
+            <header className='header-upper '>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className='col-2 text-center'>
+                            <Link to="/">
+                                <img src={Logo} alt="logo" className='img-fluid logo  align-items-center' />
+                            </Link>
+                        </div>
+                        <div className='col-3 d-flex align-items-center'>
+                            <div className="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">All</span>
+                                <input type="text" className="form-control p-2" placeholder="Search Product" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                                <button className="input-group-text" id="basic-addon2">Search</button>
+                            </div>
+                        </div>
+                        <div className='col-4 d-flex align-items-center justify-content-between text-center m-auto'>
+                            <Link to={'/'}>Home</Link>
+                            <Link to={'shop'}>Shop</Link>
+                            <Link to={'blog'}>Blog</Link>
+                            <Link to={'about'}>About</Link>
+                            <Link to={'contact'}>Contact</Link>
+
+                        </div>
+                        <div className='col-3 d-flex align-items-center justify-content-between'>
+                            <Link></Link>
+                            <Link></Link>
+                            <Link></Link>
+                        </div>
+                    </div>
+
+                </div>
+
+            </header>
+
         </>
     )
 }
