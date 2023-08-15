10ec4240698b2cd1d857318ee87c6100a1cedbfc
Abhishek280999 <darwekar155@gmail.com>
2023-08-15 13:04:18 +0530

    header

---
+++ ecommerce_app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <header className='header-top-strip p-3'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className=' col-6'>
+                            <p> The trending outfits at 100% off</p>
+                        </div>
+                        <div className='col-6'>
+                            <a href='tel:+42455555'> Call us +565485158412</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
