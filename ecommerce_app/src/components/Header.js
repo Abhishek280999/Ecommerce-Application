4d7d4f3af971c3a053650f336d9b870fe0f2f928
Abhishek280999 <darwekar155@gmail.com>
2023-08-15 13:27:55 +0530

    header 10% done

--- ecommerce_app/src/components/Header.js
+++ ecommerce_app/src/components/Header.js
@@ -1,20 +1,31 @@
 import React from 'react'
+import { BiPhoneCall } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Header = () => {
     return (
         <>
-            <header className='header-top-strip p-3'>
+            <header className='header-top-strip p-1 shadow-sm'>
                 <div className='container-xxl'>
-                    <div className='row'>
+                    <div className='row align-item-center'>
                         <div className=' col-6'>
                             <p> The trending outfits at 100% off</p>
                         </div>
-                        <div className='col-6'>
-                            <a href='tel:+42455555'> Call us +565485158412</a>
+                        <div className='col-6 d-flex justify-content-between'>
+                            <div>
+                                <a href='tel:+42455555'>Call us +565485158412</a>
+                            </div>
+                            <div>
+                                <Link><BiPhoneCall className='fs-3 mx-4' /></Link>
+                                <Link><AiOutlineMail className='fs-3 mx-4'/></Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </header>
+
+            
         </>
     )
 }
