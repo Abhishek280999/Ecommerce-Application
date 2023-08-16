25c262f6b5e0d50ffe7662bfaf45411bbb788965
Abhishek280999 <darwekar155@gmail.com>
2023-08-16 16:10:10 +0530

    Home section 3 comp

--- ecommerce_app/src/Pages/Home.js
+++ ecommerce_app/src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Hero from '../components/Hero'
+import Featured_Products from '../components/Featured_Products'
 
 const Home = () => {
   return (
@@ -19,7 +20,20 @@ const Home = () => {
         </div>
       </section>
 
-      <Hero/>
+      <Hero />
+
+      <section className='featured-products p-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='text-center'>
+              <h1>Featured Products</h1>
+              <p>All Weather New Modern Designs</p>
+            </div>
+            <Featured_Products/>
+
+          </div>
+        </div>
+      </section>
 
 
     </>
