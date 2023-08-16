d8084d098eaa3db00bc83e6dd9b579c0ce41af76
Abhishek280999 <darwekar155@gmail.com>
2023-08-16 12:53:51 +0530

    home section 1 comp

--- ecommerce_app/src/Pages/Home.js
+++ ecommerce_app/src/Pages/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
+      <section className='banner p-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='banner-details p-5 d-flex flex-column align-items-start justify-content-center'>
+              <span className='mx-3'>Trade in offer!!</span>
+              <h1>Super Value Deals</h1>
+              <h2>On all products</h2>
+              <p className=''>Save more today with Apneck</p>
+              <Link className='btn btn-primary'>Shop Now</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
       
     </>
   )
